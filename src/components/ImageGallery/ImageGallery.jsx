9ea0af2a12090752cbef4216a8f0a0107e24dbfb
Bohdan Kushnerov <bohdan.kushnerov@gmail.com>
2023-03-30 23:03:54 +0300

    Add button load more

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -7,19 +7,59 @@ export class ImageGallery extends Component {
     images: null,
     error: null,
     status: 'idle',
+    page: null,
+  };
+
+  loadImg = async () => {
+    const response = await axios.get(
+      `https://pixabay.com/api/?q=${this.props.imgTheme}&page=${this.state.page}&key=33648762-c4caeb57f8348b72b000e69b2&image_type=photo&orientation=horizontal&per_page=12`
+    );
+
+    this.setState(prevState => {
+      return {
+        page: prevState.page + 1,
+        images: [...prevState.images, ...response.data.hits],
+        status: 'resolved',
+      };
+    });
+
+    // this.setState(prevState => {
+    //   return {
+    //     page: prevState.page + 1,
+    //     images: prevState.images
+    //       ? [...prevState.images, ...response.data.hits]
+    //       : [...response.data.hits],
+    //     status: 'resolved',
+    //   };
+    // });
   };
 
   async componentDidUpdate(prevProps, prevState) {
-    console.log('prevProps', prevProps);
-    if (prevProps.imgTheme !== this.props.imgTheme) {
+    // console.log('prevProps', prevProps);
+    if (this.props.imgTheme && prevProps.imgTheme !== this.props.imgTheme) {
       this.setState({ status: 'pending' });
 
       try {
+        this.setState({
+          page: 1,
+        });
+
         const response = await axios.get(
-          `https://pixabay.com/api/?q=${this.props.imgTheme}&page=1&key=33648762-c4caeb57f8348b72b000e69b2&image_type=photo&orientation=horizontal&per_page=12`
+          `https://pixabay.com/api/?q=${this.props.imgTheme}&page=${this.state.page}&key=33648762-c4caeb57f8348b72b000e69b2&image_type=photo&orientation=horizontal&per_page=12`
         );
 
-        this.setState({ images: response.data.hits, status: 'resolved' });
+        this.setState(prevState => {
+          return {
+            page: prevState.page + 1,
+          };
+        });
+
+        this.setState({
+          images: response.data.hits,
+          status: 'resolved',
+        });
+
+        // console.log(this.state.images.length);
       } catch (error) {
         this.setState({ error, status: 'rejected' });
       }
@@ -30,11 +70,17 @@ export class ImageGallery extends Component {
     const { status } = this.state;
     if (status === 'resolved') {
       return (
-        <ul className="gallery">
-          {this.state.images.map(image => {
-            return <ImageGalleryItem key={image.id} image={image} />;
-          })}
-        </ul>
+        <>
+          <ul className="gallery">
+            {this.state.images.map(image => {
+              return <ImageGalleryItem key={image.id} image={image} />;
+            })}
+          </ul>
+
+          <button type="button" onClick={() => this.loadImg()}>
+            Load More
+          </button>
+        </>
       );
     }
   }
