ef5a64f32c80b73512e841004145863d1a3ee8ac
Bohdan Kushnerov <bohdan.kushnerov@gmail.com>
2023-04-05 12:24:10 +0300

    Refactor request

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,37 @@
 import { Component } from 'react';
-import { Searchbar } from './Searchbar/Searchbar';
+// import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Container } from './App.styled';
 import { MyGlobalStyles } from 'globalStyles/GlobalStyles.styled';
 import { ToastContainer } from 'react-toastify';
 
 export class App extends Component {
-  state = {
-    imgTheme: '',
-  };
+  // state = {
+  //   imgTheme: '',
+  //   images: [],
+  //   page: 1,
+  // };
 
-  handleFormSubmit = imgTheme => {
-    this.setState({ imgTheme });
-  };
+  // handleFormSubmit = imgTheme => {
+  //   this.setState({ imgTheme });
+  //   this.setState({ images: [] });
+  //   this.setState({ page: 1 });
+  // };
+
+  // попробуй функц яка передасть значения по дефолту: тему, [], 1
 
   render() {
     return (
       <div>
         <Container>
           <MyGlobalStyles />
-          <Searchbar onSubmit={this.handleFormSubmit} />
-          <ImageGallery imgTheme={this.state.imgTheme} />
+          {/* <Searchbar onSubmit={this.handleFormSubmit} /> */}
+          <ImageGallery
+          // imgTheme={this.state.imgTheme}
+          // resetImg={this.state.images}
+          // resetPage={this.state.page}
+          // resetState={this.state}
+          />
           <ToastContainer />
         </Container>
       </div>
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
 import { Loader } from 'components/Loader/Loader';
 import { fetchImg } from 'services/Api';
@@ -7,6 +7,8 @@ import { ImageGalleryList, ButtonLoadMore } from './ImageGallery.styled';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import { Searchbar } from 'components/Searchbar/Searchbar';
+
 const Status = {
   IDLE: 'idle',
   PENDING: 'pending',
@@ -14,110 +16,228 @@ const Status = {
   REJECTED: 'rejected',
 };
 
-export class ImageGallery extends Component {
-  static propTypes = {
-    imgTheme: PropTypes.string.isRequired,
-  };
+export function ImageGallery() {
+  const [images, setImages] = useState([]);
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState(Status.IDLE);
+  const [page, setPage] = useState(1);
+  const [totalHits, setTotalHits] = useState(null);
 
-  state = {
-    images: [],
-    error: null,
-    status: Status.IDLE,
-    page: null,
-    totalHits: null,
-  };
+  const [search, setSearch] = useState('');
 
-  async componentDidUpdate(prevProps, prevState) {
-    if (
-      this.state.status === Status.RESOLVED &&
-      this.state.images.length === this.state.totalHits &&
-      prevState.images.length !== this.state.totalHits
-    ) {
-      toast.warn('Sorry, there are no images matching your search query.');
+  useEffect(() => {
+    if (!search) {
+      return;
     }
-    const { imgTheme } = this.props;
 
-    if (imgTheme && prevProps.imgTheme !== imgTheme) {
-      await this.setState({
-        images: [],
-        page: 1,
-      });
+    async function loadImg() {
+      setStatus(Status.PENDING);
 
-      await this.loadImg();
-    }
-  }
+      try {
+        const { hits, totalHits } = await fetchImg(search, page);
 
-  loadImg = async () => {
-    this.setState({ status: Status.PENDING });
+        const normalizedImages = hits.map(
+          ({ id, webformatURL, largeImageURL, tags }) => {
+            return {
+              id,
+              webformatURL,
+              largeImageURL,
+              tags,
+            };
+          }
+        );
 
-    try {
-      const { hits, totalHits } = await fetchImg(
-        this.props.imgTheme,
-        this.state.page
-      );
-
-      await this.setState(prevState => {
-        return {
-          images: [...prevState.images, ...normalizedImages],
-          error: null,
-          status: Status.RESOLVED,
-          page: prevState.page + 1,
-          totalHits: totalHits,
-        };
-      });
-
-      const normalizedImages = hits.map(
-        ({ id, webformatURL, largeImageURL, tags }) => {
-          return {
-            id,
-            webformatURL,
-            largeImageURL,
-            tags,
-          };
+        if (page === 1) {
+          setImages([...normalizedImages]); //
+        } else {
+          setImages(prevState => [...prevState, ...normalizedImages]); //
         }
-      );
 
-      this.scrollToBottom();
-    } catch (error) {
-      this.setState({ error: 'error', status: Status.REJECTED });
+        setError(null);
+        setStatus(Status.RESOLVED);
+        setTotalHits(totalHits);
+
+        // this.scrollToBottom();
+      } catch (error) {
+        setError('error');
+        setStatus(Status.REJECTED);
+      }
     }
+    loadImg();
+
+    // return () => {
+    //   controller.abort();
+    // };
+  }, [search, page]);
+
+  // scrollToBottom = () => {
+  //   window.scrollTo({
+  //     top: document.documentElement.scrollHeight,
+  //     behavior: 'smooth',
+  //   });
+  // };
+
+  const handleFormSubmit = searchValue => {
+    if (searchValue === search && searchValue !== '') {
+      return;
+    }
+    setSearch(searchValue);
+    setImages([]);
+    setPage(1);
   };
 
-  scrollToBottom = () => {
-    window.scrollTo({
-      top: document.documentElement.scrollHeight,
-      behavior: 'smooth',
-    });
-  };
+  console.log('images.length', images.length);
+  console.log('totalHits', totalHits);
 
-  render() {
-    const { status, images, totalHits } = this.state;
+  // if (images.length === totalHits) {
+  //   toast.warn('Sorry, there are no images matching your search query.');
+  // }
 
-    return (
-      <>
-        {images.length > 0 && (
-          <ImageGalleryList className="gallery">
-            {images.map(image => {
-              return <ImageGalleryItem key={image.id} image={image} />;
-            })}
-          </ImageGalleryList>
-        )}
-        {!(images.length >= totalHits) && status === Status.RESOLVED && (
-          <ButtonLoadMore type="button" onClick={() => this.loadImg()}>
-            Load More
-          </ButtonLoadMore>
-        )}
-        {status === Status.PENDING && <Loader />}
-        {status === Status.REJECTED && (
-          <h2>
-            An error occurred, we could not upload the photo, please try
-            reloading the page and try again :)
-          </h2>
-        )}
-        {images.length === 0 && status === Status.RESOLVED && (
-          <h2>We didn't find anything according to your request</h2>
-        )}
-      </>
-    );
-  }
+  return (
+    <>
+      <Searchbar onSubmit={handleFormSubmit} />
+      {images.length > 0 && (
+        <ImageGalleryList className="gallery">
+          {images.map(image => {
+            return <ImageGalleryItem key={image.id} image={image} />;
+          })}
+        </ImageGalleryList>
+      )}
+      {!(images.length >= totalHits) && status === Status.RESOLVED && (
+        <ButtonLoadMore
+          type="button"
+          onClick={() => setPage(prevState => prevState + 1)}
+        >
+          Load More
+        </ButtonLoadMore>
+      )}
+      {status === Status.PENDING && <Loader />}
+      {status === Status.REJECTED && (
+        <h2>
+          An error occurred, we could not upload the photo, please try reloading
+          the page and try again :)
+        </h2>
+      )}
+      {/* add totalHits */}
+      {images.length === 0 && status === Status.RESOLVED && totalHits === 0 && (
+        <h2>We didn't find anything according to your request</h2>
+      )}
+    </>
+  );
 }
+
+// We didn't find anything according to your request
+
+ImageGallery.propTypes = {
+  // imgTheme: PropTypes.string.isRequired,
+  // resetImg
+  // resetPage
+};
+
+// export class ImageGallery extends Component {
+//   static propTypes = {
+//     imgTheme: PropTypes.string.isRequired,
+//   };
+
+//   state = {
+//     images: [],
+//     error: null,
+//     status: Status.IDLE,
+//     page: null,
+//     totalHits: null,
+//   };
+
+//   async componentDidUpdate(prevProps, prevState) {
+//     if (
+//       this.state.status === Status.RESOLVED &&
+//       this.state.images.length === this.state.totalHits &&
+//       prevState.images.length !== this.state.totalHits
+//     ) {
+//       toast.warn('Sorry, there are no images matching your search query.');
+//     }
+//     const { imgTheme } = this.props;
+
+//     if (imgTheme && prevProps.imgTheme !== imgTheme) {
+//       await this.setState({
+//         images: [],
+//         page: 1,
+//       });
+
+//       await this.loadImg();
+//     }
+//   }
+
+//   loadImg = async () => {
+//     this.setState({ status: Status.PENDING });
+
+//     try {
+//       const { hits, totalHits } = await fetchImg(
+//         this.props.imgTheme,
+//         this.state.page
+//       );
+
+//       await this.setState(prevState => {
+//         return {
+//           images: [...prevState.images, ...normalizedImages],
+//           error: null,
+//           status: Status.RESOLVED,
+//           page: prevState.page + 1,
+//           totalHits: totalHits,
+//         };
+//       });
+
+//       const normalizedImages = hits.map(
+//         ({ id, webformatURL, largeImageURL, tags }) => {
+//           return {
+//             id,
+//             webformatURL,
+//             largeImageURL,
+//             tags,
+//           };
+//         }
+//       );
+
+//       this.scrollToBottom();
+//     } catch (error) {
+//       this.setState({ error: 'error', status: Status.REJECTED });
+//     }
+//   };
+
+//   scrollToBottom = () => {
+//     window.scrollTo({
+//       top: document.documentElement.scrollHeight,
+//       behavior: 'smooth',
+//     });
+//   };
+
+//   render() {
+//     const { status, images, totalHits } = this.state;
+
+//     return (
+//       <>
+//         {images.length > 0 && (
+//           <ImageGalleryList className="gallery">
+//             {images.map(image => {
+//               return <ImageGalleryItem key={image.id} image={image} />;
+//             })}
+//           </ImageGalleryList>
+//         )}
+//         {!(images.length >= totalHits) && status === Status.RESOLVED && (
+//           <ButtonLoadMore type="button" onClick={() => this.loadImg()}>
+//             Load More
+//           </ButtonLoadMore>
+//         )}
+//         {status === Status.PENDING && <Loader />}
+//         {status === Status.REJECTED && (
+//           <h2>
+//             An error occurred, we could not upload the photo, please try
+//             reloading the page and try again :)
+//           </h2>
+//         )}
+//         {images.length === 0 && status === Status.RESOLVED && (
+//           <h2>We didn't find anything according to your request</h2>
+//         )}
+//       </>
+//     );
+//   }
+// }
